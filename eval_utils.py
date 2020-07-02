a6072e269887ee05eb8c5a97ee39993d6ac004df
micts <tsiaousis.michail@gmail.com>
2020-07-02 17:37:44 +0200

    removed eval_utils.py

--- eval_utils.py
+++
@@ -1,7 +0,0 @@
-import sklearn.metrics
-
-sklearn.metrics.recall_score(y_true, y_pred, pos_label=1)
-sklearn.metrics.precision_score(y_true, y_pred, pos_label=1)
-
-sklearn.metrics.roc_auc_score(y_true, (-1) * y_score)
-sklearn.metrics.average_precision_score(y_true, (-1) * y_score, pos_label=1)
