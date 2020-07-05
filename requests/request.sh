2c6b74a9af5cb0e0ea992a510eefb8ca15aa8d21
micts <tsiaousis.michail@gmail.com>
2020-07-05 23:45:13 +0200

    added requests

--- requests/request.sh
+++ requests/request.sh
@@ -1,9 +1,9 @@
 curl -i -H "Content-Type: application/json" -X POST \
-	-d '{"CPU": 27.26, 
-             "RxKBTot": 1.74, 
-	     "TxKBTot": 1.26, 
-	     "WriteKBTot": 0.0, 
-	     "RMS": 0.433838, 
-	     "diff_encoder_l": 12.354838, 
-	     "Volts": 156.025235, 
-             "R/T(xKBTot)": 1.2123}' 127.0.0.1:5000/predict
+	-d '{"CPU": 26.0, 
+             "RxKBTot": 0.16, 
+	     "TxKBTot": 0.0, 
+	     "WriteKBTot": 1.68, 
+	     "RMS": 0.051649, 
+	     "diff_encoder_l": 25.034476, 
+	     "Volts": 203.633387, 
+             "R/T(xKBTot)": 1.16}' 127.0.0.1:5000/predict
